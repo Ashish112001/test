c07149c758d80aa5888c87263bba522453fa01a5
Ashish112001 <72960900+Ashish112001@users.noreply.github.com>
2020-10-16 10:47:25 +0530

    Update head.py

--- head.py
+++ head.py
@@ -2,7 +2,7 @@ import socket
 from _thread import *
 import threading
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server = '172.30.89.113'
+server = ''
 port = 8000
 server_ip = socket.gethostbyname(server)
 
